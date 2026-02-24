ccd264ae5749c0c606910aec1a5b0f78f45da82e
viniciuspzippy <viniciusp.zippy@gmail.com>
2026-02-24 20:51:06 -0300

    Update .env.js

--- public/.env.js
+++ public/.env.js
@@ -1,8 +1,8 @@
 //.env.js
 // Este arquivo substitui o uso do .env para variáveis de ambiente no frontend
-window._env_ = {
-  REACT_APP_CHATWOOT_TOKEN: 'xxxxxxzxxxxxxxxxxxxxx',
-  REACT_APP_CHATWOOT_ACCOUNT_ID: '1',
-  REACT_APP_CHATWOOT_URL: 'https://xxxxxxxxxxx.com',
-  REACT_APP_DEBUG: 'true',
-};
+  window._env_ = {
+    REACT_APP_CHATWOOT_TOKEN: 'SEU_TOKEN_AQUI',
+    REACT_APP_CHATWOOT_ACCOUNT_ID: '1',
+    REACT_APP_CHATWOOT_URL: 'https://chatwoot.zippydigital.com.br',
+    REACT_APP_DEBUG: 'false'
+  };
