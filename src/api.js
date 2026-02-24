c0e1a97c498768f5a79fbde54cdfe4114c8f3aaf
viniciuspzippy <viniciusp.zippy@gmail.com>
2026-02-24 20:27:59 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,14 +1,9 @@
 import { debugLog } from './debug';
-const env = window._env_ || {};
-  const CHATWOOT_URL = env.REACT_APP_CHATWOOT_URL ||
-  process.env.REACT_APP_API_URL || '';
-  const ACCOUNT_ID = env.REACT_APP_CHATWOOT_ACCOUNT_ID ||
-  process.env.REACT_APP_ACCOUNT_ID || '1';
-  const TOKEN = env.REACT_APP_CHATWOOT_TOKEN ||
-  process.env.REACT_APP_CHATWOOT_TOKEN || '';
-  const chatwootHeaders = {
-    'Content-Type': 'application/json',
-    'api_access_token': TOKEN
+  window._env_ = {
+    REACT_APP_CHATWOOT_TOKEN: 'H9bu8h3Afx4Y86fsjyNdSAr4',
+    REACT_APP_CHATWOOT_ACCOUNT_ID: '1',
+    REACT_APP_CHATWOOT_URL: 'https://chatwoot.zippydigital.com.br',
+    REACT_APP_DEBUG: 'false'
   };
 async function chatwootFetch(endpoint, options = {}) {
   const url = `${CHATWOOT_URL}/api/v1/accounts/${ACCOUNT_ID}${endpoint}`;
